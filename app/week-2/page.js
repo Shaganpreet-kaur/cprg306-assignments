ed5906d062ad42efa5226d234232ab1b63ca797c
Shaganpreet-kaur <shaganpreetk632@gmail.com>
2025-01-27 10:45:23 -0700

    code for week2

---
+++ app/week-2/page.js
@@ -0,0 +1,10 @@
+import StundentInfo from "./student-info";
+
+export default function Page() {
+    return (
+      <main>
+        <h1>Shopping List</h1>
+        <StundentInfo/>
+      </main>
+    );
+  }
---
+++ app/week-2/student-info.js
@@ -0,0 +1,9 @@
+import Link from "next/link";
+export default function StundentInfo(){
+    return (
+        <div>
+        <p>Shaganpreet Kaur</p>
+        <p><Link href="https://github.com/Shaganpreet-kaur?tab=repositories">My repositories</Link></p>
+        </div>
+    );
+}
